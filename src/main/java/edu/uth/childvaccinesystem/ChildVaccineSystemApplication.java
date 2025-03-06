450670877c4fa669c919186987e7d1a206c167c1
nguyenxuanhai-arch <hainx0952@ut.edu.vn>
2025-03-06 18:01:17 +0700

    Initial commit

---
+++ src/main/java/edu/uth/childvaccinesystem/ChildVaccineSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.childvaccinesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildVaccineSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildVaccineSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uth/childvaccinesystem/ChildVaccineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.childvaccinesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildVaccineSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
