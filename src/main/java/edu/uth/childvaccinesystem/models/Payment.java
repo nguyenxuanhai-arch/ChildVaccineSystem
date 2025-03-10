95069f4a01741d9e38f2154d74617a3008a865ca
ngocnhan09022005 <nhantn0506@ut.edu.vn>
2025-03-10 14:19:34 +0700

    update models

--- src/main/java/edu/uth/childvaccinesystem/models/Payment.java
+++ src/main/java/edu/uth/childvaccinesystem/models/Payment.java
@@ -1,21 +1,70 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 package edu.uth.childvaccinesystem.models;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
+@Entity
 public class Payment {
-    private int id;
-    private int bookingId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String payerName;
     private double amount;
+    private String paymentMethod;
     private String paymentDate;
 
-    public Payment(int id, int bookingId, double amount, String paymentDate) {
-        this.id = id;
-        this.bookingId = bookingId;
+    // Constructors
+    public Payment() {}
+
+    public Payment(String payerName, double amount, String paymentMethod, String paymentDate) {
+        this.payerName = payerName;
         this.amount = amount;
+        this.paymentMethod = paymentMethod;
         this.paymentDate = paymentDate;
     }
 
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPayerName() {
+        return payerName;
+    }
+
+    public void setPayerName(String payerName) {
+        this.payerName = payerName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
 }
--- src/main/java/edu/uth/childvaccinesystem/models/User.java
+++ src/main/java/edu/uth/childvaccinesystem/models/User.java
@@ -1,42 +1,46 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 package edu.uth.childvaccinesystem.models;
 
-public class User {
-    private String id;
-    private String name;
-    private String email;
-    private String password;
 
-    // Constructor
-    public User(String id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    // Constructors
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     // Getters and Setters
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -45,5 +49,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
