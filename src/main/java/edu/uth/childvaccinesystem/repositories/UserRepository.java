bd0355df85a64318a30a1aa73da0e4ec0d476187
nguyenxuanhai-arch <hainx0952@ut.edu.vn>
2025-03-13 19:55:18 +0700

    update

--- src/main/java/edu/uth/childvaccinesystem/repositories/UserRepository.java
+++ src/main/java/edu/uth/childvaccinesystem/repositories/UserRepository.java
@@ -1,10 +1,10 @@
-package edu.uth.childvaccinesystem.reponsitories;
+package edu.uth.childvaccinesystem.repositories;
 
 import edu.uth.childvaccinesystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserReponsitory extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByPhoneNumberAndEmail(String phoneNumber, String email);
 }
