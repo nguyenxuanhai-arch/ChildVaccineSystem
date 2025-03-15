95267b8c1293e231a2645537d6db404655347d2e
nguyenxuanhai-arch <hainx0952@ut.edu.vn>
2025-03-15 14:43:42 +0700

    remove unused repository and model classes for Child, Booking, Payment, and Notification

--- src/main/java/edu/uth/childvaccinesystem/repositories/ChildRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.uth.childvaccinesystem.repositories;
-
-import edu.uth.childvaccinesystem.models.Child;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChildRepository extends JpaRepository<Child, Long> {
-}
