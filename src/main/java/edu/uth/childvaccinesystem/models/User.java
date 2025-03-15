b44e35841a174835bd7ab1c8069003ad414584a9
nguyenxuanhai-arch <hainx0952@ut.edu.vn>
2025-03-15 14:43:52 +0700

    refactor User management: simplify UserController, UserService, and User model; remove unused methods and fields

--- src/main/java/edu/uth/childvaccinesystem/models/User.java
+++ src/main/java/edu/uth/childvaccinesystem/models/User.java
@@ -1,34 +1,48 @@
 package edu.uth.childvaccinesystem.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
 
-@Setter
-@Getter
 @Entity
+@Table(name = "user")
 public class User {
 
-    // Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String password;
-    private String email;
-    private String phoneNumber;
 
     // Constructors
     public User() {
     }
 
-    public User(String username, String password, String email) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
